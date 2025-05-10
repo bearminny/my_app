7d84c1a97bbb8026f26e40229145256c706f823a
bearminny <mgo718459@gmail.com>
2025-05-10 14:24:23 +0900

    test1

--- src/app/_component/Button.tsx
+++ src/app/_component/Button.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useCallback, useState } from 'react';
-import { createUser } from '../_action/user';
+
 
 
 export function AddButton(){
@@ -30,29 +30,4 @@ export function AddButton(){
              </>
     )
 
-    // async function onClick(){
-    //     try{
-    //         const newUser = await createUser({
-    //             name: 'bearminny',
-    //             email: 'gaobaby@naver.com',
-    //             password: 'password',
-    //         });
-    //         console.log(newUser);
-    //     }   catch(error){
-    //         console.error(error);
-    //     }
-    // }
-//     return(
-//         <>
-//         <input type = "text"  name = "name" id = "name" value={name} onChange={(e) => setName(e.target.value)} />
-//         <input type = "email"  name = "email" id = "email" value={email} onChange={(e) => setEmail(e.target.value)} />
-//         <input type = "password"  name = "password" id = "password" value={password} onChange={(e) => setPassword(e.target.value)} />
-//              </>
-//         <button 
-//             onClick={() => onClick()}
-//             className="bg-blue-500 text-white p-2 rounded-md"
-//             >
-//             Click me
-//         </button>
-//     );
-    }
+}
